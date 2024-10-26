ba5e8eef0a53a89e86d6e9b3bc0309937077ad1d
Aboba <eirikrinnrauthr@tutanota.com>
2024-10-26 17:38:27 +0100

    fix: add more error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use multimap::MultiMap;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs::{self};
+use std::process::exit;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Pair {
@@ -11,21 +12,28 @@ struct Pair {
 fn main() {
     let word = std::env::args().nth(1).unwrap_or_else(|| {
         println!("Please provide a word");
-        std::process::exit(1);
+        exit(1);
     });
 
-    let dict_path = "data/dictionary.json".to_owned();
-    let contents = fs::read_to_string(dict_path).expect("Could not load dictionary");
+    let dict_path = "data/dictionary.json";
 
+    let contents = fs::read_to_string(&dict_path).expect("Could not load dictionary");
     let json: Vec<Pair> = serde_json::from_str(&contents).expect("Malformed JSON");
 
     let mut word_map = MultiMap::new();
-
     for pair in &json {
-        word_map.insert(pair.word.clone(), pair.definitions.clone());
+        word_map.insert(&pair.word, &pair.definitions);
     }
 
-    for i in &word_map[&word] {
-        println!("{}\n", i);
-    }
+    match word_map.get(&word) {
+        Some(definitons) => {
+            definitons.into_iter().for_each(|definition| {
+                println!("{}\n", definition);
+            });
+        }
+        None => {
+            println!("The given word does not exist");
+            exit(1);
+        }
+    };
 }
